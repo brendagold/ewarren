463dc1c4b6253d62488c6fbf2ec2cf0f70c38b77
brendagold <petersbrenda47@gmail.com>
2020-06-05 09:48:22 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import Header from "./component/header";
+import Main from "./component/main";
+import Footer from "./component/footer";
+import Navv from "./component/nav";
+import ScrollToTop from "./component/scrollUp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navv />
+      <Header />
+      <Main />
+      <Footer />
+      <ScrollToTop />
     </div>
   );
 }
