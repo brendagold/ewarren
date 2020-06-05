cf021b8b67377aef7aad6ea9934f415fe37b435d
brendagold <petersbrenda47@gmail.com>
2020-06-05 09:59:50 +0100

    Updates

---
+++ precache-manifest.b61e4e4b26f61d3a23615feb00adeafa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ababef867e1ae1e91e492be86f0ea1a9",
+    "url": "/ewarren/index.html"
+  },
+  {
+    "revision": "a5ba756b5e0fcdb19a2b",
+    "url": "/ewarren/static/css/2.b692f1b9.chunk.css"
+  },
+  {
+    "revision": "bb1573dcf35ba7f62531",
+    "url": "/ewarren/static/css/main.ad162bef.chunk.css"
+  },
+  {
+    "revision": "a5ba756b5e0fcdb19a2b",
+    "url": "/ewarren/static/js/2.d918a492.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ewarren/static/js/2.d918a492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1573dcf35ba7f62531",
+    "url": "/ewarren/static/js/main.09e5b6be.chunk.js"
+  },
+  {
+    "revision": "aaa00e3b31469d110294",
+    "url": "/ewarren/static/js/runtime-main.f79f6e52.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ewarren/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ewarren/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ewarren/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ewarren/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ewarren/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
